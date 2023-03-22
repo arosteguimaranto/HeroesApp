e4c38e4c8150602d1c99afdbb525ce8d341ef7a9
Arostegui Maranto <arosteguimaranto@outlook.com>
2023-03-22 14:31:38 -0600

    Mantener el usuario activo

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -7,19 +7,31 @@ const initialState = {
     logged: false,
 }
 
+const init = () =>{
+
+     const user = JSON.parse(localStorage.getItem('user'));
+
+     return {
+        logged: !!user,
+        user: user,
+     }
+}
+
 export const AuthProvider = ({ children }) => {
 
-    const [authState, dispatch] = useReducer(authReducer, initialState);
+    const [authState, dispatch] = useReducer(authReducer, initialState, init);
+
     const login = (name = '') => {
+
+        const user = {id: 'ABC', name}
+
         const action = {
             type: types.login,
-            payload: {
-                id: 'ABC',
-                name: name
-            }
+            payload: user,
         }
 
-        dispatch(action)
+        localStorage.setItem('user', JSON.stringify(user));
+        dispatch(action);
     }
 
     return (
