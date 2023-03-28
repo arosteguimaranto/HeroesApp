64664ffe3a80efdaa11226f3cd2ec73d2594f254
Arostegui Maranto <arosteguimaranto@outlook.com>
2023-03-28 13:50:36 -0600

    Pruebas en el PublicRoute - Parte 2

--- tests/router/PublicRoute.test.jsx
+++ tests/router/PublicRoute.test.jsx
@@ -1,6 +1,7 @@
 import { AuthContext } from "../../src/auth/context/AuthContext";
 import { PublicRoute } from "../../src/router/PublicRoute";
-import{render, screen} from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
+import { MemoryRouter, Route, Routes } from "react-router-dom";
 
 describe('Pruebas en <PublicRoute/>', () => {
 
@@ -20,7 +21,40 @@ describe('Pruebas en <PublicRoute/>', () => {
 
         expect(screen.getByText('Ruta publica')).toBeTruthy();
         //screen.debug()
+    });
 
-    })
+    test('debe de navegar si esta autenticado', () => {
+
+        const contextValue = {
+            logged: true,
+            user: {
+                name: 'Aros',
+                id: 'ABC123'
+            }
+        }
+        render(
+            <AuthContext.Provider value={contextValue}>
+                <MemoryRouter initialEntries={['/login']}>
+
+                    <Routes>
+                        <Route path='login' element={
+                            <PublicRoute>
+                                <h1> Ruta publica </h1>
+                            </PublicRoute>
+                        } />
+                        <Route path='marvel' element={<h1> Pagina Marvel</h1>} />
+                    </Routes>
+
+                </MemoryRouter>
+            </AuthContext.Provider>
+
+        );
+                        
+        expect( screen.getByText('Pagina Marvel')).toBeTruthy();
+
+
+    });
+
+
 
 });
