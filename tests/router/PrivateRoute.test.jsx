49ce6e128738622b7224e1f489e856f8ca42d047
Arostegui Maranto <arosteguimaranto@outlook.com>
2023-03-28 14:14:05 -0600

    Pruebas en el PrivateRoute

---
+++ tests/router/PrivateRoute.test.jsx
@@ -0,0 +1,42 @@
+import { AuthContext } from "../../src/auth/context/AuthContext";
+import { PrivateRoute } from "../../src/router/PrivateRoute";
+import { render, screen } from '@testing-library/react'
+import { MemoryRouter } from "react-router-dom";
+
+
+describe('Pruebas en el <PrivateRoute />', () => {
+
+    test('debe mostrar el children si esta autenticado', () => {
+
+        Storage.prototype.setItem = jest.fn();
+
+
+
+        const contextValue = {
+            logged: true,
+            user: {
+                id: 'abc',
+                name: 'Arostegui Maranto'
+            }
+        }
+
+        render(
+            <AuthContext.Provider value={contextValue}>
+                <MemoryRouter initialEntries={['/search?q=batman']}>
+                    <PrivateRoute>
+                        <h1> Ruta privada </h1>
+                    </PrivateRoute>
+                </MemoryRouter>
+    
+            </AuthContext.Provider>
+        );
+
+        expect(screen.getByText('Ruta privada')).toBeTruthy();
+        expect(localStorage.setItem).toHaveBeenCalledWith("lastPath", "/search?q=batman");
+     })
+
+    
+    //screen.debug()
+});
+
+
