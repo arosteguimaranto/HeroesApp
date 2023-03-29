bc35270edbc9ce06f9a5c864cde8e8f35957e3ef
Arostegui Maranto <arosteguimaranto@outlook.com>
2023-03-28 20:43:14 -0600

    Pruebas en el NavBar

---
+++ tests/ui/componentes/Navbar.test.jsx
@@ -0,0 +1,14 @@
+describe('Pruebas en <NavBar />', () => {
+
+    test('debe de mostrar el nombre del usuario', () => { 
+        
+
+     });
+
+     test('debe de llamar el logout y navigate cuando se hace click en el botón ', () => { 
+        
+
+        
+      })
+    
+});
