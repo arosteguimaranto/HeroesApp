adc2cbf6b521abb506d5d46c45b2ba22d6e261c3
Arostegui Maranto <arosteguimaranto@outlook.com>
2023-03-28 23:32:29 -0600

    Pruebas en el SearchScreen

---
+++ tests/heroes/pages/SearchPage.test.jsx
@@ -0,0 +1,18 @@
+import {render, screen} from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom';
+import { SearchPage } from '../../../src/heroes/pages/SearchPage';
+
+describe('pruebas en <SearchPage/>', () => {
+
+    test('debe de mostrarse correctamente con valores por defecto', () => { 
+
+       const {container } = render(
+            <MemoryRouter>
+            <SearchPage/>
+            </MemoryRouter>
+        );
+
+       expect(container).toMatchSnapshot();
+     })
+    
+});
